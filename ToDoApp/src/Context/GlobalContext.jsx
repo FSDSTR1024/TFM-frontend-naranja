b75215870835132b5f8fb43c12c862f2351d3002
DiegoRol69 <drol@ucm.es>
2025-03-27 21:15:44 +0100

    fallo

--- ToDoApp/src/Context/GlobalContext.jsx
+++ ToDoApp/src/Context/GlobalContext.jsx
@@ -106,13 +106,13 @@ export const GlobalProvider = ({ children }) => {
 	};
 
 	const userRegisterInGuild = (guildname) => {
-		socketRef.current = io(import.meta.env.BACKEND_API_URL);
+		socketRef.current = io(import.meta.env.VITE_BACKEND_API_URL);
 		socketRef.current.emit('userRegistered', guildname, userInfo.id, userInfo.username);
 	};
 
 	const userJoinToGuild = () => {
 		//PONER EN .ENV
-		socketRef.current = io(import.meta.env.BACKEND_API_URL);
+		socketRef.current = io(import.meta.env.VITE_BACKEND_API_URL);
 		socketRef.current.emit('joinGuild', guildInfo.guildname, userInfo.id);
 
 		socketRef.current.on('newUser', (userId, username) => {
